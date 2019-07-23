11b40f0a4d881e16cf339a6172240b3fc443f431
yqhuang1 <yqhuang1@qq.com>
2019-07-23 14:16:25 +0800

    新

--- app/src/main/java/com/example/mvvmapplication/mychat/MyWordViewModel.java
+++ app/src/main/java/com/example/mvvmapplication/mychat/MyWordViewModel.java
@@ -47,12 +47,25 @@ public class MyWordViewModel extends BaseViewModel<MyChatActivity> {
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "有啊"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "要请我吃饭嘛^_^"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "正有此意"));
-        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "学校旁友刚开了个pizza店，据说味道还不错，要不要去尝尝"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "学校旁边刚开了个pizza店，据说味道还不错，要不要去尝尝"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "你请客什么都好说"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "好哒~"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "爱你"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "么么哒~"));
         data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", ">_>"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "吃完饭去看电影嘛？~"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "这个我请客......~"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "什么电影？>_>"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "我要看喜剧片"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "听说最近上映的 《天气之子》 口碑不错~"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "是动画片吧o(*￣︶￣*)o"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "不过，你请客你说了算吧"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "几点的，要早点回，不然宿舍关门了"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "就看9点的吧，看完刚刚好~"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "GOOD IDEA！就这么安排。"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_LEFT, R.drawable.head_image_01, "张婧", "那6点在学校大门口等你"));
+        data.add(new MessageViewModel(mActivity).setData(TYPE_MESSAGE_RIGHT, R.drawable.head_image_02, "王琳琳", "ok~"));
+
         return data;
     }
 
